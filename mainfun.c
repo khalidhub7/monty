bed0d890d6c5976946afc45f757fe2a3a78f4c73
khalidhub7 <louguikhalid7@gmail.com>
2023-12-14 03:21:10 -0800

    up

--- mainfun.c
+++ mainfun.c
@@ -37,3 +37,46 @@ stack_t *createnod(int m)
 	no->n = m;
 	return (no);
 }
+
+/**
+ * free1 - Free nodes in stack
+ */
+void free1(void)
+{
+        stack_t *tmpe;
+
+        if (head == NULL)
+                return;
+
+        while (head != NULL)
+        {
+                tmpe = head;
+                head = head->next;
+                free(tmpe);
+        }
+}
+
+
+/**
+ * addtoqu - Add node to queue.
+ * @newno: Ptr to new node.
+ * @lne: line number of opcode.
+ */
+void addtoqu(stack_t **newno, __attribute__((unused))unsigned int lne)
+{
+        stack_t *tmpe;
+
+        if (newno == NULL || *newno == NULL)
+                exit(EXIT_FAILURE);
+        if (head == NULL)
+        {
+                head = *newno;
+                return;
+        }
+        tmpe = head;
+        while (tmpe->next != NULL)
+                tmpe = tmpe->next;
+
+        tmpe->next = *newno;
+        (*newno)->prev = tmpe;
+}
